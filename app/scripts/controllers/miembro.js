6a7975f8d838ef3ac44f1b01f9375d3a4d87afce
davidporras89 <davidporras89@gmail.com>
2018-09-10 17:13:19 -0500

    Mostrando Miembros

--- app/scripts/controllers/miembro.js
+++ app/scripts/controllers/miembro.js
@@ -8,10 +8,13 @@
  * Controller of the cursoJesusCondeApp
  */
 angular.module('cursoJesusCondeApp')
-  .controller('MiembroCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MiembroCtrl', function ($scope,$http) {
+    $http.get('http://localhost:9000/miembros.json').success(function(data){
+      $scope.miembros = data
+
+    })
+    $scope.gridOptions={
+      data:'miembros'
+    };
+
   });
