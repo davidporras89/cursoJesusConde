e1459a07111c8b1b8a998698556a6d781f945911
davidporras89 <davidporras89@gmail.com>
2018-09-10 08:43:31 -0500

    Creación del proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cursoJesusCondeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cursoJesusCondeApp
+ */
+angular.module('cursoJesusCondeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
